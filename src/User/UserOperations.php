e65b85244c760fe37fca80be3f08fe2fa674b717
m.alizadeh <m.alizadeh@ip-jam.com>
2023-02-09 15:18:35 +0330

    updated package

---
+++ src/User/UserOperations.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace EloquentTraits\User;
+
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @property string|int id
+ * @property string password
+ * @property string second_password
+ */
+trait UserOperations
+{
+    private string $secondPasswordSuffix = 'second';
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helper Methods
+    |--------------------------------------------------------------------------
+    */
+
+    public function setUserPassword($password): static
+    {
+        $this->password = Hash::make(
+            $password . $this->id
+        );
+        return $this;
+    }
+
+    public function checkUserPassword($password): bool
+    {
+        return Hash::check(
+            $password . $this->id,
+            $this->password
+        );
+    }
+
+    public function setUserSecondPassword($password): static
+    {
+        $this->second_password = Hash::make(
+            $password . $this->id . str($this->secondPasswordSuffix)->reverse()
+        );
+        return $this;
+    }
+
+    public function checkUserSecondPassword($password): bool
+    {
+        return Hash::check(
+            $password . $this->id . str($this->secondPasswordSuffix)->reverse(),
+            $this->second_password
+        );
+    }
+
+    public function setUserIsAdmin(): static
+    {
+        $this->privileges_code = Hash::make(
+            $this->id . "-is-admin"
+        );
+        return $this;
+    }
+
+    public function clearUserIsAdmin(): static
+    {
+        $this->privileges_code = null;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Virtual Attributes
+    |--------------------------------------------------------------------------
+    */
+
+    public function getIsAdminAttribute(): bool
+    {
+        return Hash::check(
+            $this->id . "-is-admin",
+            $this->privileges_code
+        );
+    }
+}
